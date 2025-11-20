96209f94050430d0e0e4bcecf3dbb62e80143bc6
Giovanni Dueck <giodueck@gmail.com>
2025-11-20 15:18:21 -0300

    Add Neuland and Loma Plata tiles for testing and verifying predictions

---
+++ package/config/test_products.py
@@ -0,0 +1,4 @@
+PRODUCTS = {
+    'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland
+    'T20KRA_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1228c9c5-a508-45b6-8637-4e01e47ad35a)/$value', # Loma Plata
+}
