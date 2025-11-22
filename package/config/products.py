040e258294da8ddb3c5e9ba4bd64b978d77ad41d
Giovanni Dueck <giodueck@gmail.com>
2025-11-22 18:10:19 -0300

    Add a testing tile in the Medanos del Chaco region

--- package/config/products.py
+++ package/config/products.py
@@ -2,6 +2,7 @@ train = {
     # October 2025
     'T20KNA_20251003T174955': 'https://download.dataspace.copernicus.eu/odata/v1/Products(c9c0a608-1ed5-41ee-98bb-2346ff1502d2)/$value',
     'T20KNB_20251003T174955': 'https://download.dataspace.copernicus.eu/odata/v1/Products(81e3454b-cf13-493b-b4ef-a8370ee4900f)/$value',
+    # September 2025
     'T20KPC_20250913T194708': 'https://download.dataspace.copernicus.eu/odata/v1/Products(44342712-af91-4a2c-b434-9fa5553684a4)/$value',
     # June 2025
     'T20KNA_20250620T173703': 'https://download.dataspace.copernicus.eu/odata/v1/Products(e4cebb62-b507-4e8a-86e7-63986518b673)/$value',
@@ -21,6 +22,7 @@ train = {
 test = {
     'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland
     'T20KRA_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1228c9c5-a508-45b6-8637-4e01e47ad35a)/$value', # Loma Plata and Filadelfia
+    'T20KNC_20250913T194708': 'https://download.dataspace.copernicus.eu/odata/v1/Products(7327d41c-8b46-4814-9b84-cf67f865000c)/$value', # Western-most part of Medanos, not all included in training
 }
 
 neuland = {
@@ -31,9 +33,14 @@ lp_fila = {
     'T20KRA_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1228c9c5-a508-45b6-8637-4e01e47ad35a)/$value', # Loma Plata and Filadelfia
 }
 
+medanos = {
+    'T20KNC_20250913T194708': 'https://download.dataspace.copernicus.eu/odata/v1/Products(7327d41c-8b46-4814-9b84-cf67f865000c)/$value', # Western-most part of Medanos, not all included in training
+}
+
 PRODUCTS = {
     'train': train,
     'test': test,
     'neuland': neuland,
     'lp_fila': lp_fila,
+    'medanos': medanos,
 }
