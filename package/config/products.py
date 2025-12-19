b11d0e176b6d2b337320177212e5539bae571314
Giovanni Dueck <giodueck@gmail.com>
2025-12-19 20:40:58 -0300

    Snapshot of experimentation

--- package/config/products.py
+++ package/config/products.py
@@ -17,17 +17,49 @@ train = {
     'T20KNA_20241003T200351': 'https://download.dataspace.copernicus.eu/odata/v1/Products(4033655f-4ddf-4f0c-93aa-e5f9c13746ae)/$value',
     'T20KNB_20241003T200351': 'https://download.dataspace.copernicus.eu/odata/v1/Products(cc08a79a-3f8e-4894-99f3-6922ff6ca549)/$value',
     'T20KPC_20241020T181904': 'https://download.dataspace.copernicus.eu/odata/v1/Products(9fbca04e-8190-463a-a40d-bddb986248af)/$value',
+    # August 2022
+    'T20KNA_20240714T072909': 'https://download.dataspace.copernicus.eu/odata/v1/Products(2cf29f45-39a8-4363-921f-d5fbe8428f40)/$value',
+    'T20KNB_20240714T072909': 'https://download.dataspace.copernicus.eu/odata/v1/Products(2d6c7cc2-ce75-4bfd-820e-5eda12cb3983)/$value',
+    'T20KPC_20240714T072909': 'https://download.dataspace.copernicus.eu/odata/v1/Products(758d1703-802e-4d1a-bb5f-078285f047f0)/$value',
+    # August 2016
+    'T20KPC_20231022T025928': 'https://download.dataspace.copernicus.eu/odata/v1/Products(25d4fbb7-2b63-496b-9ec6-b7662e189d8a)/$value',
+    # April 2016
+    'T20KNA_20231012T132858': 'https://download.dataspace.copernicus.eu/odata/v1/Products(a15f2694-9826-4f75-a68a-05467c5fc8f7)/$value',
+    # December 2015
+    'T20KNA_20231016T100753': 'https://download.dataspace.copernicus.eu/odata/v1/Products(d6cb71fb-6a9c-4cc1-8a73-a75a7b3732bf)/$value',
+
+    # Neuland
+    # 'T20KQV_20231010T081418': 'https://download.dataspace.copernicus.eu/odata/v1/Products(91e70f7c-4b56-4ef2-a53e-b3f8ba4397ed)/$value', # August 2015
+    # 'T20KQV_20250603T212126': 'https://download.dataspace.copernicus.eu/odata/v1/Products(035a3888-4e5a-4eba-9ffa-40eecc9b42fb)/$value', # September 2015
+    # 'T20KQV_20240711T132134': 'https://download.dataspace.copernicus.eu/odata/v1/Products(fdbedd36-1e3f-48c6-b87a-a7c70e429d29)/$value', # Neuland august 2022 (drought)
+    # 'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland spring
+    # 'T20KQV_20251209T174649': 'https://download.dataspace.copernicus.eu/odata/v1/Products(bea32373-fc07-40e4-930e-7adc133e92b0)/$value', # Neuland summer
+    # 'T20KQV_20250722T194121': 'https://download.dataspace.copernicus.eu/odata/v1/Products(e6e6dc31-f140-42c3-8242-1965c67d301c)/$value', # Neuland winter
+
+    # Loma Plata and Filadelfia
+    # 'T20KRA_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1228c9c5-a508-45b6-8637-4e01e47ad35a)/$value', # Loma Plata and Filadelfia
+    # 'T20KRA_20240711T132134': 'https://download.dataspace.copernicus.eu/odata/v1/Products(b73549da-fdfa-4c3d-bd21-08f9c84cc94b)/$value', # August 2022
+    # 'T20KRA_20230810T003314': 'https://download.dataspace.copernicus.eu/odata/v1/Products(2c589427-92a2-46c2-8fe4-d0a5890fe45b)/$value', # August 2018
+    # 'T20KRA_20230726T173450': 'https://download.dataspace.copernicus.eu/odata/v1/Products(dda76ae9-d25f-4d3e-8836-15d9bedb524f)/$value', # December 2018
 }
 
 test = {
     'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland
     'T20KRA_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1228c9c5-a508-45b6-8637-4e01e47ad35a)/$value', # Loma Plata and Filadelfia
     'T20KNC_20250913T194708': 'https://download.dataspace.copernicus.eu/odata/v1/Products(7327d41c-8b46-4814-9b84-cf67f865000c)/$value', # Western-most part of Medanos, not all included in training
+    'T20KNC_20231022T025928': 'https://download.dataspace.copernicus.eu/odata/v1/Products(22a4d641-db30-47c0-ade0-c5a57b87bb1e)/$value', # same, but august 2016
 }
 
 neuland = {
-    'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland
-    'T20KQV_20251209T174649': 'https://download.dataspace.copernicus.eu/odata/v1/Products(bea32373-fc07-40e4-930e-7adc133e92b0)/$value', # Neuland
+    # 'T20KQV_20251020T174731': 'https://download.dataspace.copernicus.eu/odata/v1/Products(665ca9a2-9f9f-457b-95b7-4629d24dcde8)/$value', # Neuland spring
+    # 'T20KQV_20251209T174649': 'https://download.dataspace.copernicus.eu/odata/v1/Products(bea32373-fc07-40e4-930e-7adc133e92b0)/$value', # Neuland summer
+    # 'T20KQV_20250722T194121': 'https://download.dataspace.copernicus.eu/odata/v1/Products(e6e6dc31-f140-42c3-8242-1965c67d301c)/$value', # Neuland winter
+    # 'T20KQV_20240801T210252': 'https://download.dataspace.copernicus.eu/odata/v1/Products(d9e397a0-46ca-4ee6-a3d8-b5fb17047356)/$value', # Neuland august 2024
+    # 'T20KQV_20241027T131250': 'https://download.dataspace.copernicus.eu/odata/v1/Products(bd5ebdba-747f-403d-acb2-b310d66d5995)/$value', # Neuland august 2023
+    # 'T20KQV_20240711T132134': 'https://download.dataspace.copernicus.eu/odata/v1/Products(fdbedd36-1e3f-48c6-b87a-a7c70e429d29)/$value', # Neuland august 2022 (drought)
+    'T20KQV_20231010T081418': 'https://download.dataspace.copernicus.eu/odata/v1/Products(91e70f7c-4b56-4ef2-a53e-b3f8ba4397ed)/$value', # August 2015
+    'T20KQV_20250603T212126': 'https://download.dataspace.copernicus.eu/odata/v1/Products(035a3888-4e5a-4eba-9ffa-40eecc9b42fb)/$value', # September 2015
+    'T20KQV_20231002T173436': 'https://download.dataspace.copernicus.eu/odata/v1/Products(5b0cbdaa-81f8-4e46-b2e1-804933ea746d)/$value', # August 2016
 }
 
 lp_fila = {
@@ -35,23 +67,21 @@ lp_fila = {
 }
 
 mariscal = {
-    'T20KQA_20251206T172309': 'https://download.dataspace.copernicus.eu/odata/v1/Products(9ac99d36-5738-4ea5-8fc4-ada19d122014)/$value',
+    'T20KQA_20251005T193014': 'https://download.dataspace.copernicus.eu/odata/v1/Products(4b066aa3-1c37-4595-a6e8-2215a0053f81)/$value',
+    # 'T20KQA_20251206T172309': 'https://download.dataspace.copernicus.eu/odata/v1/Products(9ac99d36-5738-4ea5-8fc4-ada19d122014)/$value',
 }
 
 medanos = {
     'T20KNC_20250913T194708': 'https://download.dataspace.copernicus.eu/odata/v1/Products(7327d41c-8b46-4814-9b84-cf67f865000c)/$value', # Western-most part of Medanos, not all included in training
 }
 
-mbaracayu = {
-    'T21KXP_20251120T171211': 'https://download.dataspace.copernicus.eu/odata/v1/Products(e7c87e45-c5fe-454a-aa4c-12fb2bae186b)/$value', # Reserva Natural Bosque Mbaracayu
-}
-
-ybyturuzu = {
-    'T21JWM_20251120T171211': 'https://download.dataspace.copernicus.eu/odata/v1/Products(4973f12b-68d5-4c6a-b7c6-df9b73877e5b)/$value' # Reserva de recursos manejados Ybyturuzu
-}
-
 pilar = {
-    'T21JUL_20251126T173437': 'https://download.dataspace.copernicus.eu/odata/v1/Products(b8f88f8f-9151-4844-af8e-cb4ad392a1b4)/$value' # Pilar
+    'T21JUL_20251126T173437': 'https://download.dataspace.copernicus.eu/odata/v1/Products(b8f88f8f-9151-4844-af8e-cb4ad392a1b4)/$value', # Pilar
+}
+
+argentina = {
+    'T21JTL_20240626T075907': 'https://download.dataspace.copernicus.eu/odata/v1/Products(1a24c7e1-a3d1-4bf4-8867-cc165aa6a3ac)/$value', # Gral San Martin, Argentina, August 2022
+    'T21JUL_20251126T173437': 'https://download.dataspace.copernicus.eu/odata/v1/Products(b8f88f8f-9151-4844-af8e-cb4ad392a1b4)/$value', # Pilar
 }
 
 PRODUCTS = {
@@ -60,8 +90,7 @@ PRODUCTS = {
     'neuland': neuland,
     'lp_fila': lp_fila,
     'medanos': medanos,
-    'mbaracayu': mbaracayu,
-    'ybyturuzu': ybyturuzu,
     'mariscal': mariscal,
     'pilar': pilar,
+    'argentina': argentina,
 }
